0e045cd77434db020514dc883f0eedc3e478953a
张亚飞 <672233580@qq.com>
2021-07-06 14:54:03 +0800

    Initial Commit

---
+++ SwiftUIBaseCategory/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  SwiftUIBaseCategory
+//
+//  Created by 张亚飞 on 2021/7/6.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ SwiftUIBaseCategory/SwiftUIBaseCategoryApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftUIBaseCategoryApp.swift
+//  SwiftUIBaseCategory
+//
+//  Created by 张亚飞 on 2021/7/6.
+//
+
+import SwiftUI
+
+@main
+struct SwiftUIBaseCategoryApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
